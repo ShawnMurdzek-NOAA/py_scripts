fix bugs with P2 calculation

synthetic_obs/create_conv_obs_wgts_load.py
@@ -58,11 +58,7 @@ wrf_dir = work + '/BMC/wrfruc/murdzek/nature_run_spring_v2/'
 #wrf_dir = work + '/BMC/wrfruc/murdzek/nature_run_tests/nature_run_spring_v2/output/202204291200/UPP/'
 
 # Directory containing real prepbufr CSV output
-bufr_dir = './'
-
-# File containing conventional observation error characteristics (use GSI errtable file)
-error_fname = work + '/BMC/wrfruc/murdzek/sample_real_obs/errtable.rrfs'
-add_errors = False
+bufr_dir = work + '/BMC/wrfruc/murdzek/sample_real_obs/obs_rap/'
 
 # Optional path to pickle containing KDTree for WRF (lat, lon) grid
 use_pkl = True
@@ -412,8 +408,14 @@ for i in range(ntimes):
         for hr in wrf_hr: 
 
             # Determine indices of obs within wrf_step of this output time
-            ind = np.where(np.logical_and(out_df['DHR'] > (hr - wrf_step_dec), 
-                                          out_df['DHR'] <= (hr + wrf_step_dec)))
+            if hr == wrf_hr[1]:
+                # Include obs that occur right at the first WRF hour, otherwise the P2 conditional
+                # below does not execute, which means that pwgt and pi0 are not computed
+                ind = np.where(np.logical_and(out_df['DHR'] >= (hr - wrf_step_dec), 
+                                              out_df['DHR'] <= (hr + wrf_step_dec)))
+            else:
+                ind = np.where(np.logical_and(out_df['DHR'] > (hr - wrf_step_dec), 
+                                              out_df['DHR'] <= (hr + wrf_step_dec)))
             ind = np.intersect1d(ind, np.array(ob_idx['ADPUPA'] + ob_idx['AIRCAR'] + ob_idx['AIRCFT']))
 
             # If no indices, move to next time
@@ -460,7 +462,7 @@ for i in range(ntimes):
 
                     if debug > 1:
                         time2 = dt.datetime.now()
-                        print('done determining twgt (%.6f s)' % (time2 - time1).total_seconds())
+                        print('done determining twgt (%.3f, %.6f s)' % (twgt, (time2 - time1).total_seconds()))
 
                     # Interpolate horizontally (code here is adapted from cou.wrf_coords)
                     lat = out_df.loc[j, 'YOB']
@@ -533,10 +535,10 @@ for i in range(ntimes):
                     # have height. The calculation below only gives use part of the p1d array. The 
                     # rest of this of this array will be determined when we loop over the other 3D
                     # pressure array
-                    p1d[:, j] = twgt * (xwgt1 * ywgt1 * wrf3d[:, yi0, xi0] +
-                                        xwgt2 * ywgt1 * wrf3d[:, yi0, xi0+1] +
-                                        xwgt1 * ywgt2 * wrf3d[:, yi0+1, xi0] +
-                                        xwgt2 * ywgt2 * wrf3d[:, yi0+1, xi0+1])
+                    p1d[:, j] = twgt * 1e-2 * (xwgt1 * ywgt1 * wrf3d[:, yi0, xi0] +
+                                               xwgt2 * ywgt1 * wrf3d[:, yi0, xi0+1] +
+                                               xwgt1 * ywgt2 * wrf3d[:, yi0+1, xi0] +
+                                               xwgt2 * ywgt2 * wrf3d[:, yi0+1, xi0+1])
  
                     if debug > 1:
                         time5 = dt.datetime.now()
@@ -572,11 +574,19 @@ for i in range(ntimes):
                                                              xwgt1 * ywgt2 * wrf3d[:, yi0+1, xi0] +
                                                              xwgt2 * ywgt2 * wrf3d[:, yi0+1, xi0+1]))
 
-                    pi0 = np.where(p1d[:, j] > out_df.loc[j, 'POB'])[0][-1]
-                    pwgt = (p1d[pi0+1, j] - out_df.loc[j, 'POB']) / (p1d[pi0+1, j] - p1d[pi0, j])
+                    # Check for extrapolation
+                    if (p1d[0, j] > out_df.loc[j, 'POB']):
+                        pi0 = np.where(p1d[:, j] > out_df.loc[j, 'POB'])[0][-1]
+                        pwgt = (p1d[pi0+1, j] - out_df.loc[j, 'POB']) / (p1d[pi0+1, j] - p1d[pi0, j])
+                    else:
+                        drop_idx.append(j)
+                        continue
 
                     # Interpolate POB
                     out_df.loc[j, 'POB'] = (p1d[pi0, j]**pwgt)*(p1d[pi0+1, j]**(1.-pwgt))
+                    if debug > 1: 
+                        print('interpolated P = %.2f' % out_df.loc[j, 'POB'])
+                        print('actual P = %.2f' % bufr_csv.df.loc[j, 'POB'])
 
                     # save some stuff
                     out_df.loc[j, 'pwgt']  = pwgt 
@@ -610,12 +620,13 @@ for i in range(ntimes):
                               xwgt2 * ywgt1 * wrf3d[pi0+1, yi0, xi0+1] +
                               xwgt1 * ywgt2 * wrf3d[pi0+1, yi0+1, xi0] +
                               xwgt2 * ywgt2 * wrf3d[pi0+1, yi0+1, xi0+1])
-                        out_df.loc[j, o] = out_df.loc[j, o] + (v1**pwgt) * (v2**(1.-pwgt))
+                        if np.isclose(out_df.loc[j, o], 0):
+                            out_df.loc[j, o] = twgt * (v1**pwgt) * (v2**(1.-pwgt))
+                        else:
+                            out_df.loc[j, o] = out_df.loc[j, o] + (1.-twgt) * (v1**pwgt) * (v2**(1.-pwgt))
                         if debug > 1:
                             time7 = dt.datetime.now()
                             print('finished interp for %s (%.6f s)' % (o, (time7 - time6).total_seconds()))
-
-                    # Add errors
 
                     if debug > 1:
                         print('total time = %.6f s' % (dt.datetime.now() - time1).total_seconds())

synthetic_obs/plot_ob_diffs_vprof.py
@@ -4,7 +4,7 @@ Plot Observation Differences Between Two Prepbufr CSV
 This script should ideally be used to compare synthetic obs vs. real obs
 
 shawn.s.murdzek@noaa.gov
-Date Created: 7 February 2023
+Date Created: 14 February 2023
 """
 
 #---------------------------------------------------------------------------------------------------
@@ -14,8 +14,6 @@ Date Created: 7 February 2023
 import bufr
 import matplotlib.pyplot as plt
 import numpy as np
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
 
 
 #---------------------------------------------------------------------------------------------------
@@ -26,11 +24,11 @@ import cartopy.feature as cfeature
 fname1 = '/mnt/lfs4/BMC/wrfruc/murdzek/nature_run_spring_v2/synthetic_obs/202204291200.fake.prepbufr.csv'
 fname2 = '/mnt/lfs4/BMC/wrfruc/murdzek/nature_run_spring_v2/synthetic_obs/202204291200.real_red.prepbufr.csv'
 
-# Output file name (include %s placeholder for variable)
-save_fname = './ob_diffs_%s.png'
+# Output file name
+save_fname = './ob_diffs_vprof.png'
 
 # Observation subsets
-subsets = ['SHPSFC', 'ADPSFC', 'MSONET']
+subsets = ['ADPUPA']
 
 # Variables to plot
 obs_vars = ['ELV', 'POB', 'TOB', 'QOB', 'UOB', 'VOB', 'ZOB']
@@ -55,36 +53,34 @@ ind = np.where(boo)
 bufr_df1.df = bufr_df1.df.loc[ind]
 bufr_df2.df = bufr_df2.df.loc[ind]
 
-lat = bufr_df1.df['YOB'].values
-lon = bufr_df1.df['XOB'].values
+pres = bufr_df1.df['POB'].values
 
-for v in obs_vars:
+fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(8, 6), sharey=True)
+plt.subplots_adjust(left=0.1, bottom=0.1, right=0.95, top=0.9, hspace=0.3, wspace=0.3)
+for i, v in enumerate(obs_vars):
     print('Plotting %s' % v)
-    fig = plt.figure(figsize=(10, 6))
-    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    ax = axes[int(i/4), i%4]
 
     diff = bufr_df1.df[v] - bufr_df2.df[v]
-    maxdiff = np.amax(np.abs(diff))
 
-    cax = ax.scatter(lon, lat, s=75, c=diff, cmap='bwr', vmin=-maxdiff, vmax=maxdiff,
-                     transform=ccrs.PlateCarree(), edgecolors='k', linewidths=0.5)
-    cbar = plt.colorbar(cax, ax=ax, orientation='horizontal')
-    cbar.set_label('%s diffs (%s)' % (v, bufr_df1.meta[v]['units']), size=14)
+    ax.plot(diff, np.log10(pres), 'bo')
+    ax.axvline(0, c='k', lw='1')
+    ax.grid()
 
-    ax.coastlines('50m')
-    borders = cfeature.NaturalEarthFeature(category='cultural',
-                                           scale='50m',
-                                           facecolor='none',
-                                           name='admin_1_states_provinces')
-    ax.add_feature(borders, linewidth=0.5, edgecolor='k')
-    ax.set_xlim([-135, -50])
-    ax.set_ylim([20, 55])
-    ax.set_title(title, size=18)
+    ax.set_ylim([np.log10(1050), np.log10(100)])
+    ticks = np.array([1000, 850, 700, 500, 400, 300, 200, 100])
+    ax.set_yticks(np.log10(ticks))
+    ax.set_yticklabels(ticks)
+    ax.set_xlabel('%s (%s)' % (v, bufr_df1.meta[v]['units']), size=12)
 
-    plt.savefig(save_fname % v)
-    plt.close()
+for i in range(2):
+    axes[i, 0].set_ylabel('pressure (%s)' % bufr_df1.meta['POB']['units'], size=12)
+
+plt.suptitle(title, size=16)
+plt.savefig(save_fname)
+plt.close()
 
 
 """
-End plot_ob_diffs_2d.py  
+End plot_ob_diffs_vprof.py  
 """