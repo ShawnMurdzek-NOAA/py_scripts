16a947c3abd1fe1dfe911fa9ec42f68b8d6317de
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2023-04-12 10:58:21 -0500

    add station IDs to plot_sfc_station_locs.py

--- NR_eval/plot_sfc_station_locs.py
+++ NR_eval/plot_sfc_station_locs.py
@@ -21,8 +21,9 @@ import matplotlib.pyplot as plt
 
 # Surface station files
 station_files = []
-path = '/scratch1/BMC/wrfruc/murdzek/sample_real_obs/sfc_stations'
-for s in ['OAK', 'TUS', 'GJT', 'DTW', 'ABR', 'ALB', 'MIA', 'BHM', 'CRP', 'SLE']:
+station_names = ['OAK', 'TUS', 'GJT', 'DTW', 'ABR', 'ALB', 'MIA', 'BHM', 'CRP', 'SLE']
+path = '/work2/noaa/wrfruc/murdzek/real_obs/sfc_stations'
+for s in station_names:
     station_files.append('%s/%s_202204290000_202205070000.txt' % (path, s))
 
 # Output file
@@ -33,11 +34,13 @@ out_fname = 'sfc_station_locs.png'
 # Plot Surface Station Locations
 #---------------------------------------------------------------------------------------------------
 
-fig = plt.figure(figsize=(8, 5))
+fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-for f in station_files:
+for f, s in zip(station_files, station_names):
     df = pd.read_csv(f, skiprows=5)
     ax.plot(df['lon'].loc[0], df['lat'].loc[0], 'bo', markersize=10, transform=ccrs.PlateCarree())
+    ax.text(df['lon'].loc[0]+0.5, df['lat'].loc[0]+0.5, s, fontweight='bold', 
+            transform=ccrs.PlateCarree())
 
 ax.set_extent([-130, -65, 20, 50])
 ax.set_title('Surface Station Locations', size=18)
