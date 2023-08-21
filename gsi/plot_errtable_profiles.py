9f44b7e4980ace4f6d25dc06bf172ae80085a958
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2023-08-21 12:02:21 -0500

    Feat: Add plot_errtable_profiles.py
    Added a script to plot vertical profiles of GSI errtable standard deviations

---
+++ gsi/plot_errtable_profiles.py
@@ -0,0 +1,92 @@
+"""
+Plot GSI Error Table Profiles
+
+One plot is created for each observation type, with one subplot for each variable. Multiple error
+tables can be plotted at once.
+
+shawn.s.murdzek@noaa.gov
+"""
+
+#---------------------------------------------------------------------------------------------------
+# Import Modules
+#---------------------------------------------------------------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+import pyDA_utils.gsi_fcts as gsi
+
+
+#---------------------------------------------------------------------------------------------------
+# Input Parameters
+#---------------------------------------------------------------------------------------------------
+
+etable_std_fnames = ['/work2/noaa/wrfruc/murdzek/real_obs/errtable.rrfs',
+                     '/work2/noaa/wrfruc/murdzek/src/osse_ob_creator/utils/errtable.tmp',
+                     '/work2/noaa/wrfruc/murdzek/src/osse_ob_creator/utils/errtable_72hr.tmp']
+etable_mean_fnames = ['/work2/noaa/wrfruc/murdzek/real_obs/errtable_mean.rrfs',
+                      '/work2/noaa/wrfruc/murdzek/src/osse_ob_creator/utils/errtable_mean.tmp',
+                      '/work2/noaa/wrfruc/murdzek/src/osse_ob_creator/utils/errtable_mean_72hr.tmp']
+etable_labels = ['original', '7 days', '3 days']
+etable_colors = ['k', 'b', 'r']
+etable_linestyles = ['-', '-', '--']
+
+out_fname = './errtable_profiles.pdf'
+
+
+#---------------------------------------------------------------------------------------------------
+# Make Plots
+#---------------------------------------------------------------------------------------------------
+
+print('Reading in error tables...')
+err_df = {}
+#for stat, l in zip(['std', 'mean'], [etable_std_fnames, etable_mean_fnames]):
+for stat, l in zip(['std'], [etable_std_fnames]):
+    err_df[stat] = {}
+    for fname, label in zip(l, etable_labels):
+        err_df[stat][label] = gsi.read_errtable(fname)    
+
+print('Making plots...')
+errs = ['Terr', 'RHerr', 'UVerr', 'PSerr', 'PWerr']
+pdf = PdfPages(out_fname)
+for typ in err_df['std'][etable_labels[0]].keys():
+
+    print(typ)
+
+    # Skip this observation if all errors are NaN
+    skip = True
+    for label in etable_labels:
+        for e in errs:
+            if not np.all(np.isnan(err_df['std'][label][typ][e])):
+                skip = False
+                break
+    if skip:
+        continue
+
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8), sharey=True)
+    plt.subplots_adjust(left=0.07, bottom=0.08, right=0.98, top=0.92)
+    for j, e in enumerate(errs):
+        ax = axes[int(j/3), j%3]
+        for label, c, ls in zip(etable_labels, etable_colors, etable_linestyles):
+            for stat in ['std']:
+                ax.plot(err_df[stat][label][typ][e], err_df[stat][label][typ]['prs'], c=c, ls=ls,
+                        label=label)
+        ax.legend()
+        ax.grid()
+        ax.set_yscale('log')
+        ax.set_ylim([1100, 10])
+        ax.set_xlabel(e, size=14)
+        ax.set_xlim(left=0)
+    plt.suptitle('Type = %d' % typ, size=20)
+    for j in range(2):
+        axes[j, 0].set_ylabel('pressure (mb)', size=14)        
+
+    pdf.savefig(fig)
+    plt.close(fig)
+pdf.close()
+
+
+"""
+End plot_errtable_profiles.py
+"""
