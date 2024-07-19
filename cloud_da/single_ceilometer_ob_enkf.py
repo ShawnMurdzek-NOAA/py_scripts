ed567c4f59481427f9bb31945d765c7b6c5777b8
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-07-19 21:59:17 +0000

    Add postage stamp plots to single-ob ceilometer EnKF code

--- cloud_da/single_ceilometer_ob_enkf.py
+++ cloud_da/single_ceilometer_ob_enkf.py
@@ -10,6 +10,10 @@ shawn.s.murdzek@noaa.gov
 
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
+import cartopy
+import cartopy.crs as ccrs
+import datetime as dt
 
 import probing_rrfs_ensemble as pre
 import pyDA_utils.cloud_DA_forward_operator as cfo
@@ -45,7 +49,7 @@ def run_cld_forward_operator_1ob(ens_obj, param, ens_name=['mem0001'], hofx_kw={
         hofx_output[i] = cld_hofx.data['hofx'][0][param['ob_idx']]
         cld_amt = cld_hofx.data['ob_cld_amt'][0][param['ob_idx']]
     
-    return cld_amt, hofx_output
+    return cld_amt, hofx_output, cld_ob_df
 
 
 def unravel_state_matrix(x, ens_obj):
@@ -63,7 +67,116 @@ def unravel_state_matrix(x, ens_obj):
     return output
 
 
+def add_inc_and_analysis_to_ens_obj(ens_obj, enkf_obj):
+    """
+    Add the analysis increment and analysis fields to the ens_obj for easier plotting
+    """
+
+    # Compute increment
+    inc_1d = enkf_obj.x_a - enkf_obj.x_b
+
+    # Turn 1D fields into nD fields
+    inc_nd = unravel_state_matrix(inc_1d, ens_obj)
+    xa_nd = unravel_state_matrix(enkf_obj.x_a, ens_obj)
+
+    # Add fields to ens_obj
+    for out, label in zip([inc_nd, xa_nd], ['incr_', 'ana_']):
+        for v in out.keys():
+            for ens in out[v].keys():
+                ens_obj.subset_ds[ens][label+v] = ens_obj.subset_ds[ens][v].copy()
+                ens_obj.subset_ds[ens][label+v].values = out[v][ens]
+
+    return ens_obj
+
+
+def plot_horiz_postage_stamp(ens_obj, param, upp_field='TCDC_P0_L105_GLC0', klvl=0, 
+                             ob={'plot':False}):
+    """
+    Make horizontal cross section postage stamp plots
+
+    Parameters
+    ----------
+    ens_obj : pyDA_utils.ensemble_utils.ensemble
+        Ensemble object
+    param : dictionary
+        YAML inputs
+
+    Returns
+    -------
+    fig : plt.figure
+        Plot
+
+    """
+
+    # Field name and colorbar limits/colormap. Set klvl to NaN if 2D field
+    extend = 'both'
+    if (upp_field == 'TCDC_P0_L105_GLC0') or (upp_field == 'ana_TCDC_P0_L105_GLC0'):
+        if upp_field == 'TCDC_P0_L105_GLC0':
+            save_fname = f"{param['out_dir']}/postage_stamp_cloud_cover_bgd_{param['save_tag']}.png"
+        else:
+            save_fname = f"{param['out_dir']}/postage_stamp_cloud_cover_ana_{param['save_tag']}.png"
+        title = 'Cloud Cover'
+        cmap = 'plasma_r'
+        clevels = np.arange(0, 100.1, 5)
+        extend = 'neither'
+    elif upp_field == 'incr_TCDC_P0_L105_GLC0':
+        save_fname = f"{param['out_dir']}/postage_stamp_cloud_cover_incr_{param['save_tag']}.png"
+        title = 'Cloud Cover'
+        cmap = 'bwr'
+        clevels = np.arange(-97.5, 97.6, 5)
+
+    elif (upp_field == 'TMP_P0_L105_GLC0') or (upp_field == 'ana_TMP_P0_L105_GLC0'):
+        if upp_field == 'TMP_P0_L105_GLC0':
+            save_fname = f"{param['out_dir']}/postage_stamp_T_bgd_{param['save_tag']}.png"
+        else:
+            save_fname = f"{param['out_dir']}/postage_stamp_T_ana_{param['save_tag']}.png"
+        title = 'Temperature'
+        cmap = 'plasma'
+        clevels = np.arange(250, 270, 1)
+    elif upp_field == 'incr_TMP_P0_L105_GLC0':
+        save_fname = f"{param['out_dir']}/postage_stamp_T_incr_{param['save_tag']}.png"
+        title = 'Temperature'
+        cmap = 'bwr'
+        clevels = np.arange(-5.75, 5.76, 0.5)
+    
+    elif (upp_field == 'SPFH_P0_L105_GLC0') or (upp_field == 'ana_SPFH_P0_L105_GLC0'):
+        if upp_field == 'SPFH_P0_L105_GLC0':
+            save_fname = f"{param['out_dir']}/postage_stamp_Q_bgd_{param['save_tag']}.png"
+        else:
+            save_fname = f"{param['out_dir']}/postage_stamp_Q_ana_{param['save_tag']}.png"
+        title = 'Specific Humidity'
+        cmap = 'plasma'
+        clevels = np.arange(0, 5e-3, 2.4e-4)
+        extend = 'max'
+    elif upp_field == 'incr_SPFH_P0_L105_GLC0':
+        save_fname = f"{param['out_dir']}/postage_stamp_Q_incr_{param['save_tag']}.png"
+        title = 'Specific Humidity'
+        cmap = 'bwr'
+        clevels = np.arange(-5.75, 5.76, 0.5) * 1e-4
+
+    nrows = 5
+    ncols = 6
+    figsize = (12, 10)
+
+    # Make plot
+    fig = ens_obj.postage_stamp_contourf(upp_field, nrows, ncols, klvl=klvl, figsize=figsize, title=title,
+                                         plt_kw={'ingest_kw':{'zind':[klvl]}, 
+                                                 'cntf_kw':{'cmap':cmap, 'levels':clevels, 'extend':extend}})
+    
+    # Add location of observation
+    if ob['plot']:
+        for ax in fig.axes:
+            if type(ax) == cartopy.mpl.geoaxes.GeoAxes:
+                ax.plot(ob['x'], ob['y'], transform=ccrs.PlateCarree(), **ob['kwargs'])
+
+    plt.savefig(save_fname)
+
+    return fig
+
+
 if __name__ == '__main__':
+
+    start = dt.datetime.now()
 
     # YAML inputs
     yml_fname = sys.argv[1]
@@ -86,10 +199,11 @@ if __name__ == '__main__':
     ens_obj = pre.preprocess_obs_ceil(ens_obj)
 
     # Apply cloud DA forward operator
-    cld_amt, hofx = run_cld_forward_operator_1ob(ens_obj, param, 
-                                                 ens_name=ens_obj.mem_names,
-                                                 hofx_kw={'hgt_lim_kw':{'max_hgt':3500},
-                                                          'verbose':0})
+    cld_amt, hofx, cld_ob_df = run_cld_forward_operator_1ob(ens_obj, param, 
+                                                            ens_name=ens_obj.mem_names,
+                                                            hofx_kw={'hgt_lim_kw':{'max_hgt':3500},
+                                                                     'verbose':0},
+                                                            verbose=False)
     print('Cloud ceilometer H(x) =', hofx)
     print('Cloud ceilometer ob =', cld_amt)
 
@@ -97,9 +211,21 @@ if __name__ == '__main__':
     enkf_obj = enkf.enkf_1ob(ens_obj.state_matrix['data'], cld_amt, hofx, param['ob_var'])
     enkf_obj.EnSRF()
 
-    # Compute analysis increments
-    inc_1d = enkf_obj.x_a - enkf_obj.x_b
-    inc_nd = unravel_state_matrix(inc_1d, ens_obj)
+    # Save output to ens_obj for easier plotting
+    ens_obj = add_inc_and_analysis_to_ens_obj(ens_obj, enkf_obj)
+
+    # Make plots
+    for meta in ['', 'incr_', 'ana_']:
+        for v in param['state_vars']:
+            print(f'plotting {meta}{v}...')
+            fig = plot_horiz_postage_stamp(ens_obj, param, upp_field=f'{meta}{v}', klvl=param['plot_klvl'],
+                                           ob={'plot':True,
+                                               'x':cld_ob_df['XOB'].values[0] - 360, 
+                                               'y':cld_ob_df['YOB'].values[0], 
+                                               'kwargs':{'marker':'*', 'color':'k'}})
+            plt.close(fig)
+    
+    print(f'total elapsed time = {(dt.datetime.now() - start).total_seconds()} s')
 
 
 """
