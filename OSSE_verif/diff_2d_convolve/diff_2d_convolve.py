b09139e90efed3420c818ceb5c34e284fa21517b
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2025-02-11 16:46:56 -0600

    Add docstrings and region matching to diff_2d_convolve

--- OSSE_verif/diff_2d_convolve/diff_2d_convolve.py
+++ OSSE_verif/diff_2d_convolve/diff_2d_convolve.py
@@ -92,6 +92,20 @@ def read_fcst_output(param, itime, fhr):
     """
     Read forecast output files
 
+    Parameters
+    ----------
+    param : dictionary
+        Program parameters
+    itime : pd.datetime
+        Model initialization time
+    fhr : integer
+        Forecast hour
+
+    Returns
+    -------
+    ds1, ds2 : xr.Dataset
+        Forecast output from the two forecasts being examined
+
     """
 
     fnames = {}
@@ -115,6 +129,24 @@ def read_fcst_output(param, itime, fhr):
 
 
 def extract_field(ds, param):
+    """
+    Extract a field from a forecast Dataset.
+    If the field is derived, compute it.
+    Convert to binary field if desired.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        Forecast output
+    param : dictionary
+        Program parameters
+
+    Returns
+    -------
+    field : np.array
+        Desired forecast field
+
+    """
 
     name = param['field']['name']
 
@@ -138,6 +170,22 @@ def extract_field(ds, param):
 
 
 def extract_lat_lon(ds, param):
+    """
+    Determine the (lat, lon) coordinate of the centroid of each patch
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        Forecast output
+    param : dictionary
+        Program parameters
+
+    Returns
+    -------
+    lat, lon : np.array
+        Coordinates of the center of each convolution patch
+
+    """
 
     c_size = param['convolve']['size']
     subset = param['convolve']['subset']
@@ -154,10 +202,26 @@ def extract_lat_lon(ds, param):
 
 
 def perform_diff_2d_convolve(field1, field2, param):
+    """
+    Compute a squared diff field, take the 2D convolution, and convert to RMSDs
 
-    diff = (field1 - field2)**2
+    Parameters
+    ----------
+    field1, field2 : np.array
+        Forecast fields to take RMSD of
+    param : dictionary
+        Program parameters
+
+    Returns
+    -------
+    rmsd : np.array
+        RMSDs between the two fields for each of the convolution patches
+
+    """
+
+    diff2 = (field1 - field2)**2
     kernel = np.ones([param['convolve']['size']]*2)
-    rmsd = np.sqrt(ss.convolve2d(diff, kernel, mode='valid') / np.sum(kernel))
+    rmsd = np.sqrt(ss.convolve2d(diff2, kernel, mode='valid') / np.sum(kernel))
 
     # Subset RMSDs
     subset = param['convolve']['subset']
@@ -167,6 +231,20 @@ def perform_diff_2d_convolve(field1, field2, param):
 
 
 def rmsd_diff_field_names(param):
+    """
+    Return the two RMSD field names when taking RMSD diffs between two forecast hours
+
+    Parameters
+    ----------
+    param : dictionary
+        Program parameters
+
+    Returns
+    -------
+    string
+        RMSD field names
+
+    """
 
     fhr1 = param['model']['fcst_hr'][0]
     fhr2 = param['model']['fcst_hr'][1]
@@ -175,6 +253,20 @@ def rmsd_diff_field_names(param):
 
 
 def init_out_dict(param):
+    """
+    Initialize the output dictionary
+
+    Parameters
+    ----------
+    param : dictionary
+        Program parameters
+
+    Returns
+    -------
+    out : dictionary
+        Dictionary used to store program output
+
+    """
 
     out = {'lat':[],
            'lon':[],
@@ -192,6 +284,30 @@ def init_out_dict(param):
 
 
 def save_output_1iter(param, rmsd, lat, lon, init, fhr, out):
+    """
+    Save output from a single initialization time
+
+    Parameters
+    ----------
+    param : dictionary
+        Program parameters
+    rmsd : np.array or list
+        RMSDs or list of RMSDs
+    lat, lon : np.array
+        Coordinates of convolution patch centers
+    init : pd.datetime
+        Initialization time
+    fhr : integer
+        Forecast hour
+    out : dictionary
+        Output dictionary
+
+    Returns
+    -------
+    out : dictionary
+        Output dictionary
+
+    """
 
     size = lat.size
     out['lat'] = out['lat'] + list(lat.flatten())
@@ -210,6 +326,22 @@ def save_output_1iter(param, rmsd, lat, lon, init, fhr, out):
 
 
 def save_output_fname(out, param):
+    """
+    Save output dictionary to a CSV
+
+    Parameters
+    ----------
+    out : dictionary
+        Output dictionary
+    param : dictionary
+        Program parameters
+
+    Returns
+    -------
+    out_df : pd.DataFrame
+        Output dictionary in DataFrame format
+
+    """
 
     # Create DataFrame
     out_df = pd.DataFrame.from_dict(out)
@@ -231,13 +363,36 @@ def save_output_fname(out, param):
     else:
         out_df.sort_values('rmsd', ascending=False, inplace=True)
 
+    # Add closest region
+    out_df = match_lat_lon_to_region(out_df)
+
     # Save to CSV
-    out_df.to_csv(fname)
+    out_df.to_csv(fname, index=False)
 
     return out_df
 
 
 def compute_rmsds_1init(init, param, out_dict, verbose=1):
+    """
+    Compute and save RMSDs for a single initialization time
+
+    Parameters
+    ----------
+    init : pd.datetime
+        Initialization time
+    param : dictionary
+        Program parameters
+    out_dict : dictionary
+        Output dictionary
+    verbose : integer, optional
+        Verbosity level (larger numbers means more output)
+
+    Returns
+    -------
+    out_dict : dictionary
+        Output dictionary
+
+    """
 
     if param['model']['rmsd_fhr_diff']:
         rmsd = []
@@ -258,6 +413,40 @@ def compute_rmsds_1init(init, param, out_dict, verbose=1):
         out_dict = save_output_1iter(param, rmsd, lat, lon, t, fhr, out_dict)
 
     return out_dict
+
+
+def match_lat_lon_to_region(df):
+    """
+    Match (lat, lon) coordinates with patches used in pygraf output
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with (lat, lon) coordinates
+
+    Returns
+    -------
+    df : pd.DataFrame
+        DataFrame with a new "regions" column that includes the closest region
+
+    """
+
+    # Center (lat, lon) coordinate for each region
+    region_lat = np.array([43.5, 42, 43.5, 32.5, 29.5, 34.75])
+    region_lon = np.array([-97, -76.5, -114, -96.5, -82.75, -112.5])
+    region = ['NC', 'NE', 'NW', 'SC', 'SE', 'SW']
+
+    # Determine indices of the minimum distance
+    dist2_all = np.zeros([len(df), len(region)])
+    for i in range(len(region)):
+        dist2_all[:, i] = ((df['lat'].values - region_lat[i])**2 + 
+                           (df['lon'].values - region_lon[i])**2)
+    idx = np.argmin(dist2_all, axis=1)
+
+    # Add closest region to df
+    df['regions'] = [region[i] for i in idx]
+
+    return df
 
 
 if __name__ == '__main__':
